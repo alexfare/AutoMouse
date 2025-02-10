ff7ade5a8d242777d826244cbf338a3418ec3b4f
Alex Fare <40654995+alexfare@users.noreply.github.com>
2025-02-09 19:07:24 -0500

    v1.0.0.0

---
+++ Main.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.btnRecord = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.btnPlay = New System.Windows.Forms.Button()
+        Me.btnStopPlayback = New System.Windows.Forms.Button()
+        Me.lstRecordedActions = New System.Windows.Forms.ListBox()
+        Me.lblCountdown = New System.Windows.Forms.Label()
+        Me.chkBlockInput = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'btnRecord
+        '
+        Me.btnRecord.Location = New System.Drawing.Point(284, 25)
+        Me.btnRecord.Name = "btnRecord"
+        Me.btnRecord.Size = New System.Drawing.Size(95, 23)
+        Me.btnRecord.TabIndex = 0
+        Me.btnRecord.Text = "Start Recording"
+        Me.btnRecord.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.Location = New System.Drawing.Point(284, 54)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(95, 23)
+        Me.btnStop.TabIndex = 1
+        Me.btnStop.Text = "Stop Recording"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'btnPlay
+        '
+        Me.btnPlay.Location = New System.Drawing.Point(284, 83)
+        Me.btnPlay.Name = "btnPlay"
+        Me.btnPlay.Size = New System.Drawing.Size(95, 23)
+        Me.btnPlay.TabIndex = 2
+        Me.btnPlay.Text = "Start Playback"
+        Me.btnPlay.UseVisualStyleBackColor = True
+        '
+        'btnStopPlayback
+        '
+        Me.btnStopPlayback.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnStopPlayback.Location = New System.Drawing.Point(284, 112)
+        Me.btnStopPlayback.Name = "btnStopPlayback"
+        Me.btnStopPlayback.Size = New System.Drawing.Size(95, 23)
+        Me.btnStopPlayback.TabIndex = 3
+        Me.btnStopPlayback.Text = "Stop Playback"
+        Me.btnStopPlayback.UseVisualStyleBackColor = True
+        '
+        'lstRecordedActions
+        '
+        Me.lstRecordedActions.FormattingEnabled = True
+        Me.lstRecordedActions.Location = New System.Drawing.Point(12, 25)
+        Me.lstRecordedActions.Name = "lstRecordedActions"
+        Me.lstRecordedActions.Size = New System.Drawing.Size(266, 303)
+        Me.lstRecordedActions.TabIndex = 4
+        '
+        'lblCountdown
+        '
+        Me.lblCountdown.AutoSize = True
+        Me.lblCountdown.Location = New System.Drawing.Point(12, 9)
+        Me.lblCountdown.Name = "lblCountdown"
+        Me.lblCountdown.Size = New System.Drawing.Size(39, 13)
+        Me.lblCountdown.TabIndex = 5
+        Me.lblCountdown.Text = "Label1"
+        '
+        'chkBlockInput
+        '
+        Me.chkBlockInput.AutoSize = True
+        Me.chkBlockInput.Location = New System.Drawing.Point(284, 157)
+        Me.chkBlockInput.Name = "chkBlockInput"
+        Me.chkBlockInput.Size = New System.Drawing.Size(102, 17)
+        Me.chkBlockInput.TabIndex = 6
+        Me.chkBlockInput.Text = "Block user input"
+        Me.chkBlockInput.UseVisualStyleBackColor = True
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(385, 331)
+        Me.Controls.Add(Me.lblCountdown)
+        Me.Controls.Add(Me.chkBlockInput)
+        Me.Controls.Add(Me.lstRecordedActions)
+        Me.Controls.Add(Me.btnRecord)
+        Me.Controls.Add(Me.btnStopPlayback)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnPlay)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "AutoMouse"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnRecord As Button
+    Friend WithEvents btnStop As Button
+    Friend WithEvents btnPlay As Button
+    Friend WithEvents btnStopPlayback As Button
+    Friend WithEvents lstRecordedActions As ListBox
+    Friend WithEvents lblCountdown As Label
+    Friend WithEvents chkBlockInput As CheckBox
+End Class
---
+++ Main.vb
@@ -0,0 +1,221 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Threading
+
+Public Class Main
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim getVersion As String = My.Settings.Version
+        Me.Text = "AutoMouse - v" + getVersion
+        lblCountdown.Text = ""
+
+        RegisterHotKey(Me.Handle, HOTKEY_ID, MOD_ALT, VK_ESCAPE) ' ALT+ESC
+    End Sub
+
+    ' API to get the cursor position
+    <DllImport("user32.dll")>
+    Public Shared Function GetCursorPos(ByRef lpPoint As Point) As Boolean
+    End Function
+
+    ' API for mouse events (for playback)
+    <DllImport("user32.dll", SetLastError:=True)>
+    Public Shared Sub mouse_event(dwFlags As UInteger, dx As UInteger, dy As UInteger, dwData As UInteger, dwExtraInfo As IntPtr)
+    End Sub
+
+    ' Block user input
+    <DllImport("user32.dll")>
+    Private Shared Function BlockInput(ByVal fBlock As Boolean) As Boolean
+    End Function
+
+    ' Hot Keys
+    <DllImport("user32.dll")>
+    Private Shared Function RegisterHotKey(hWnd As IntPtr, id As Integer, fsModifiers As Integer, vk As Integer) As Boolean
+    End Function
+
+    <DllImport("user32.dll")>
+    Private Shared Function UnregisterHotKey(hWnd As IntPtr, id As Integer) As Boolean
+    End Function
+
+    Private Const MOD_ALT As Integer = &H1
+    Private Const MOD_CONTROL As Integer = &H2
+    Private Const VK_S As Integer = &H53
+    Private Const VK_ESCAPE As Integer = &H1B
+
+    Private Const HOTKEY_ID As Integer = 1
+
+
+    ' Mouse event constants
+    Private Const MOUSEEVENTF_LEFTDOWN As UInteger = &H2
+    Private Const MOUSEEVENTF_LEFTUP As UInteger = &H4
+    Private Const MOUSEEVENTF_RIGHTDOWN As UInteger = &H8
+    Private Const MOUSEEVENTF_RIGHTUP As UInteger = &H10
+
+    ' API to detect key states
+    <DllImport("user32.dll")>
+    Private Shared Function GetAsyncKeyState(ByVal vKey As Integer) As Short
+    End Function
+
+    Private Const VK_LBUTTON As Integer = &H1
+    Private Const VK_RBUTTON As Integer = &H2
+
+    Public Class MouseAction
+        Public Property X As Integer
+        Public Property Y As Integer
+        Public Property ClickType As String
+        Public Property Delay As Integer
+
+        Public Overrides Function ToString() As String
+            Return $"{ClickType} at ({X}, {Y}) after {Delay}ms"
+        End Function
+    End Class
+
+    Private recording As Boolean = False
+    Private playing As Boolean = False
+    Private recordedActions As New List(Of MouseAction)()
+    Private lastRecordedTime As Integer
+
+    Private Sub btnRecord_Click(sender As Object, e As EventArgs) Handles btnRecord.Click
+        lblCountdown.Text = "Recording Starting in: 3"
+        lblCountdown.Refresh()
+        Thread.Sleep(1000)
+
+        lblCountdown.Text = "Recording Starting in: 2"
+        lblCountdown.Refresh()
+        Thread.Sleep(1000)
+
+        lblCountdown.Text = "Recording Starting in: 1"
+        lblCountdown.Refresh()
+        Thread.Sleep(1000)
+
+        lblCountdown.Text = "Go!"
+        lblCountdown.Refresh()
+        Thread.Sleep(500)
+
+        lblCountdown.Text = ""
+
+        recording = True
+        recordedActions.Clear()
+        lastRecordedTime = Environment.TickCount
+        lstRecordedActions.Items.Clear()
+
+        Dim recordThread As New Thread(AddressOf RecordMouse)
+        recordThread.IsBackground = True
+        recordThread.Start()
+    End Sub
+
+    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        recording = False
+    End Sub
+
+    Private Sub RecordMouse()
+        Dim lastLeftState As Boolean = False
+        Dim lastRightState As Boolean = False
+
+        While recording
+            Dim cursorPos As Point
+            GetCursorPos(cursorPos)
+
+            Dim leftState As Boolean = (GetAsyncKeyState(VK_LBUTTON) <> 0)
+            Dim rightState As Boolean = (GetAsyncKeyState(VK_RBUTTON) <> 0)
+
+            ' Record movement
+            Dim action As New MouseAction With {
+                .X = cursorPos.X,
+                .Y = cursorPos.Y,
+                .ClickType = "Move",
+                .Delay = Environment.TickCount - lastRecordedTime
+            }
+            lastRecordedTime = Environment.TickCount
+            recordedActions.Add(action)
+
+            ' Record left click
+            If leftState AndAlso Not lastLeftState Then
+                recordedActions.Add(New MouseAction With {
+                    .X = cursorPos.X,
+                    .Y = cursorPos.Y,
+                    .ClickType = "LeftClick",
+                    .Delay = 0
+                })
+            End If
+
+            ' Record right click
+            If rightState AndAlso Not lastRightState Then
+                recordedActions.Add(New MouseAction With {
+                    .X = cursorPos.X,
+                    .Y = cursorPos.Y,
+                    .ClickType = "RightClick",
+                    .Delay = 0
+                })
+            End If
+
+            lastLeftState = leftState
+            lastRightState = rightState
+
+            ' Update UI list
+            Me.Invoke(Sub() lstRecordedActions.Items.Add(action.ToString()))
+
+            Thread.Sleep(10)
+        End While
+    End Sub
+
+    ' Start Playback
+    Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+        playing = True
+        Dim playbackThread As New Thread(AddressOf PlayMouse)
+        playbackThread.IsBackground = True
+        playbackThread.Start()
+    End Sub
+
+    ' Stop Playback
+    Private Sub btnStopPlayback_Click(sender As Object, e As EventArgs) Handles btnStopPlayback.Click
+        playing = False
+    End Sub
+
+    ' Play Back Recorded Mouse Movements and Clicks
+    Private Sub PlayMouse()
+        If chkBlockInput.Checked Then
+            BlockInput(True) ' Block input
+        End If
+
+        Try
+            While playing
+                ' Create a copy of recordedActions to prevent modification during playback
+                Dim actionsToPlay As List(Of MouseAction) = Nothing
+                SyncLock recordedActions
+                    actionsToPlay = New List(Of MouseAction)(recordedActions)
+                End SyncLock
+
+                ' Loop through the copied list
+                For Each action As MouseAction In actionsToPlay
+                    If Not playing Then Exit For
+
+                    Thread.Sleep(action.Delay)
+
+                    ' Move Mouse
+                    Cursor.Position = New Point(action.X, action.Y)
+
+                    ' Simulate Clicks
+                    If action.ClickType = "LeftClick" Then
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, IntPtr.Zero)
+                        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, IntPtr.Zero)
+                    ElseIf action.ClickType = "RightClick" Then
+                        mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, IntPtr.Zero)
+                        mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, IntPtr.Zero)
+                    End If
+                Next
+            End While
+        Finally
+            BlockInput(False) ' Unblock input when stopping
+        End Try
+    End Sub
+
+    Protected Overrides Sub WndProc(ByRef m As Message)
+        Const WM_HOTKEY As Integer = &H312
+        If m.Msg = WM_HOTKEY Then
+            playing = False ' Stop playback when hotkey is pressed
+        End If
+        MyBase.WndProc(m)
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        UnregisterHotKey(Me.Handle, HOTKEY_ID)
+    End Sub
+End Class
